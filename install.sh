6e0b3a025b964d5e3c6d8c789f95e220007964f0
Rick Dutour Geerling <info@rickdg.nl>
2022-05-23 11:16:15 +0200

    fix: fix install path and install cargo if required

--- install.sh
+++ install.sh
@@ -6,22 +6,48 @@ ROVER_VERSION="v0.5.1"
 ARCHITECTURE=$(arch)
 OS=$(uname -s)
 
+has_cargo() {
+  set +e
+  if ! command -v cargo; then
+    echo 1
+  else
+    echo 0
+  fi
+  set -e
+}
+
 install_from_source () {
-  echo "Installing rover from source"
+  local -r INSTALL_CARGO=$(has_cargo)
+
+  if [ "$INSTALL_CARGO" -eq "1" ]; then
+    echo "==> Installing Rust toolchain"
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+    source $HOME/.cargo/env
+  fi
+
+  echo "==> Installing Rover from source"
 
   mkdir -p /tmp/rover
   wget -O - https://github.com/apollographql/rover/tarball/${ROVER_VERSION} | tar xz -C /tmp/rover --strip-components=1
-  cd /tmp/rover
+
+  pushd /tmp/rover
   cargo run --release -- install
+  popd
 
   mkdir -p node_modules/.bin
-  mv /tmp/rover/target/release/rover .
+  mv /tmp/rover/target/release/rover node_modules/.bin
+
+  echo "==> Cleaning up"
+  rm -Rf /tmp/rover
+  if [ "$INSTALL_CARGO" -eq "1" ]; then
+    rm -Rf $HOME/.cargo
+  fi
 }
 
 install_with_curl(){
   curl -sSL https://rover.apollo.dev/nix/$ROVER_VERSION | sh
   mkdir -p node_modules/.bin
-  mv $HOME/.rover/bin/rover .
+  mv $HOME/.rover/bin/rover node_modules/.bin
 }
 
 if [ "$ARCHITECTURE" = "aarch64" -a "$OS" = "Linux" ]; then
